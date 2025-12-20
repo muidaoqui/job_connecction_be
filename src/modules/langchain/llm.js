d986379131dbc73dcd65bbdb69b6ae0d8dd79e69
netprtony <huynhvikhang6a13@gmail.com>
2025-12-20 16:03:43 +0700

    feat: Add LLM service module and Langchain integration.

--- src/modules/langchain/llm.js
+++ src/modules/langchain/llm.js
@@ -1,24 +1,24 @@
-import { ChatOpenAI } from "@langchain/openai";
-import { FoundryLocalManager } from "foundry-local-sdk";
+// import { ChatOpenAI } from "@langchain/openai";
+// import { FoundryLocalManager } from "foundry-local-sdk";
 
-const alias = "qwen2.5-1.5b-instruct-cuda-gpu:4";
-const manager = new FoundryLocalManager();
+// const alias = "qwen2.5-1.5b-instruct-cuda-gpu:4";
+// const manager = new FoundryLocalManager();
 
-let llm;
+// let llm;
 
-export async function getLLM() {
-  if (!llm) {
-    const modelInfo = await manager.init(alias);
+// export async function getLLM() {
+//   if (!llm) {
+//     const modelInfo = await manager.init(alias);
 
-    llm = new ChatOpenAI({
-      modelName: modelInfo.id,
-      temperature: 0.7,
-      maxTokens: 400,
-      openAIApiKey: manager.apiKey,
-      configuration: {
-        baseURL: manager.endpoint
-      }
-    });
-  }
-  return llm;
-}
+//     llm = new ChatOpenAI({
+//       modelName: modelInfo.id,
+//       temperature: 0.7,
+//       maxTokens: 400,
+//       openAIApiKey: manager.apiKey,
+//       configuration: {
+//         baseURL: manager.endpoint
+//       }
+//     });
+//   }
+//   return llm;
+// }
